03ec0376c63385edf178ca3fb659ffbc4284cebf
SimRous <simonrouge1237@gmail.com>
2024-04-26 10:44:12 -0400

    roll speed boost

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -6,7 +6,9 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float vitesseDeplacement = 2.0f;
+    [SerializeField] float rollSpeed = 6.0f;
     // Start is called before the first frame update
+    public float speed;
     public Vector2 direction { get; private set; } = Vector2.zero;
     Rigidbody2D rb;
     SpriteRenderer sr;
@@ -17,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponentInChildren<Animator>();
+        speed = vitesseDeplacement;
     }
 
     // Update is called once per frame
@@ -28,9 +31,14 @@ public class PlayerMovement : MonoBehaviour
         if (direction.x < 0)
             sr.flipX = true;
         if (direction.magnitude > 0)
+        {
             Deplacement();
+            rollBoost();
+        }
+            
         else
             animator.SetBool("walking", false);
+        
 
     }
     void Deplacement()
@@ -38,7 +46,7 @@ public class PlayerMovement : MonoBehaviour
         animator.SetBool("walking", true);
         if (ShopUI.activeSelf)
             direction = new Vector2(0, 0);
-        rb.MovePosition((rb.position + direction.normalized * vitesseDeplacement * Time.fixedDeltaTime));
+        rb.MovePosition((rb.position + direction.normalized * speed * Time.fixedDeltaTime));
     }
     public bool flipped(bool notWhenYAxes = false)
     {
@@ -54,9 +62,26 @@ public class PlayerMovement : MonoBehaviour
         if(!ShopUI.activeSelf)
             direction = context.ReadValue<Vector2>();
     }
+    void rollBoost()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("roll"))
+        {
+            speed = rollSpeed;
+
+        }
+        else
+        {
+            speed = vitesseDeplacement;
+        }
+    }
     public void Roll(InputAction.CallbackContext context)
     {
+
         if (context.started)
-            animator.SetTrigger("roll");
+            if (direction.magnitude != 0)
+            {
+                animator.SetTrigger("roll");
+            }
+                
     }
 }
